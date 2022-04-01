037144fec3bf7bf72bf5ed34cad402467be9e13c
Taha Berk U <93040162+developenguin34@users.noreply.github.com>
2022-04-01 23:38:01 +0300

    Delete py2executable/src directory

--- py2executable/src/py2executable.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def linux():
-    scriptnamelinux=input("Enter the path of script :")
-    os.system('sudo -S python'+' '+scriptnamelinux)
-
-def windows():
-    scriptnamewindows=input("Enter the path of script :")
-    os.system('python'+' '+scriptnamewindows)
